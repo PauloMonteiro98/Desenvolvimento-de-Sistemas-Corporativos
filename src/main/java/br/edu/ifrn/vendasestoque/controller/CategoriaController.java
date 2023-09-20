b5ec1f5998a1550d1e923dc0a5d1892703c55ec5
Joao Maria Araujo do Nascimento <1724026@ifrn.local>
2023-09-20 08:59:47 -0300

    Adição de métodos de listar categoria

--- src/main/java/br/edu/ifrn/vendasestoque/controller/CategoriaController.java
+++ src/main/java/br/edu/ifrn/vendasestoque/controller/CategoriaController.java
@@ -1,7 +1,12 @@
 package br.edu.ifrn.vendasestoque.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +15,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.edu.ifrn.vendasestoque.domain.categoria.Categoria;
 import br.edu.ifrn.vendasestoque.repository.CategoriaRepository;
+import lombok.extern.log4j.Log4j2;
 
 @RestController
+@Log4j2
 @RequestMapping("categorias")
 public class CategoriaController {
 
@@ -25,6 +32,18 @@ public class CategoriaController {
         var uri = uriBuilder.path("/categorias/{id}").
                   buildAndExpand(categoriaLocal.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id){
+        var categoria = repository.findById(id);
+        return ResponseEntity.ok(categoria);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> listar(){
+        List<Categoria> categorias = repository.findAll();
+        return ResponseEntity.ok(categorias);
     }
     
 }
--- src/main/java/br/edu/ifrn/vendasestoque/domain/categoria/Categoria.java
+++ src/main/java/br/edu/ifrn/vendasestoque/domain/categoria/Categoria.java
@@ -1,5 +1,7 @@
 package br.edu.ifrn.vendasestoque.domain.categoria;
 
+import java.io.Serializable;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,14 +11,18 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Table(name="categoria")
 @Entity(name = "categoria")
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @EqualsAndHashCode(of = "id")
-public class Categoria {
+public class Categoria implements Serializable {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
